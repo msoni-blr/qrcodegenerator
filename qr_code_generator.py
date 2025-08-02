ad752fc8ac158c0b38769a27cc9791c981b3853b
MayankS <mayanks@mayanks.local>
2025-08-03 05:29:06 +0530

    QR code generator with custom file name

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -1,5 +1,8 @@
 import utils
 
+FILE_EXTENSION = ".png"
+FILE_PREFIX = "qrcode_img_"
+
 
 def generate_qr_code():
     while True:
@@ -8,9 +11,14 @@ def generate_qr_code():
             print("Invalid input!")
             continue
         filename = input(
-            "(Optional) Enter the file name for QR code, or hit enter: ")
+            "(Optional) Enter the file name with .png extension for QR code, or hit enter for default: ")
+
         if not filename:
-            filename = f"qrcode_img_{utils.get_datetime()}.png"
+            filename = f"{utils.get_datetime()}{FILE_EXTENSION}"
+        elif not filename.endswith(FILE_EXTENSION):
+            filename = f"{filename}{FILE_EXTENSION}"
+
+        filename = f"{FILE_PREFIX}{filename}"
         try:
             utils.generate_qr_code(data, filename)
             print(f"Successfully generated the QR code at {filename}")
